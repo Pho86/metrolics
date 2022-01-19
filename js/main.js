4d63ec45f7e65110fb31fe07a9d6e03a4da888e3
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-01-18 22:52:53 -0800

    nav + thingsofabove section layouting/some styling

--- js/main.js
+++ js/main.js
@@ -9,17 +9,17 @@ for (i = 0; i < acc.length; i++) {
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
+    if (panel.style.display === "grid") {
       panel.style.display = "none";
     } else {
-      panel.style.display = "block";
+      panel.style.display = "grid";
     }
   });
 }
 
-function openCity(cityName) {
+function tabOut123(cityName) {
    var i;
-   var x = document.getElementsByClassName("city");
+   var x = document.getElementsByClassName("tabout");
    for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
    }
